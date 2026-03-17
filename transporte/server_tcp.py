254a5830ecafcdc4f52dfb86616fa63f09ebd6d5
rafaelsilvag <rafaelsg@gmail.com>
2026-03-17 08:52:50 -0300

    fix: threads on TCP server

--- transporte/server_tcp.py
+++ transporte/server_tcp.py
@@ -1,7 +1,23 @@
 import socket
+import _thread
 
 HOST = ""  # Endereco IP do Servidor
 PORT = 5000  # Porta que o Servidor esta
+
+
+def connected(conn, client):
+    print(f"Connected by {client}")
+    while True:
+        msg = conn.recv(1024)
+        if not msg:
+            break
+        msg_decoded = msg.decode('utf-8')
+        print(f"{client} {msg_decoded}")
+        # Sending confirmation to the client
+        conn.send("ok".encode('utf-8'))
+
+    print(f"Connection ended by {client}")
+    conn.close()
 
 
 def server():
@@ -13,18 +29,7 @@ def server():
 
     while True:
         conn, client = tcp.accept()
-        print(f"Connected by {client}")
-        while True:
-            msg = conn.recv(1024)
-            if not msg:
-                break
-            msg_decoded = msg.decode('utf-8')
-            print(f"{client} {msg_decoded}")
-            # Sending confirmation to the client
-            conn.send("ok".encode('utf-8'))
-
-        print(f"Connection ended by {client}")
-        conn.close()
+        _thread.start_new_thread(connected, (conn, client))
 
 
 if __name__ == "__main__":
